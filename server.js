57c31c7a1f981773021e5215900c0f9f89624726
Eze-kiel-a <zaedwera@gmail.com>
2023-10-28 21:04:14 +0300

    Added endpoints for server.js

--- server.js
+++ server.js
@@ -14,6 +14,51 @@ app.get('/', function (request, response) {
   response.render('home');
 });
 
+app.post('/overviewPage', urlEncodedParser, function (request, response) {
+  const name = request.body.name;
+  const email = request.body.email;
+  const password = request.body.password;
+  const room = parseInt(request.body.room);
+
+  if(name && email && password && room){
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      room: room
+    };
+    //check if exists into database
+    //save the details into the database
+
+    response.render('overviewPage', user);
+
+  } else {
+    response.render('home', {
+      errorMessage: 'Details not entered!',
+    });
+  }
+});
+
+app.get('/overviewPage', urlEncodedParser, function (request, response) {
+  const email = request.body.email;
+  const password = request.body.password;
+
+  if(email && password){
+    const user = {
+      email: email,
+      password: password,
+    };
+    //check if exists in database
+    
+    response.render('overviewPage', user);
+
+  } else {
+    response.render('home', {
+      errorMessage: 'Details not entered!',
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
