40ba8363c235cbcd72c15d26566989a3af9e1775
Eze-kiel-a <zaedwera@gmail.com>
2023-10-29 20:16:42 +0300

    Test the checkNameExists function

--- validate.test.js
+++ validate.test.js
@@ -1,4 +1,4 @@
-const {checkEmailExists} = require('./validate');
+const {checkEmailExists, checkNameExists} = require('./validate');
 
 test('checkEmailExists: thisemail@usiu.ac.ke does not exist', () => {
     expect(checkEmailExists('thisemail@usiu.ac.ke')).toBe("Email doesn't exist!");
@@ -6,3 +6,10 @@ test('checkEmailExists: thisemail@usiu.ac.ke does not exist', () => {
 test('checkEmailExists: nsalim@usiu.ac.ke does exist', () => {
     expect(checkEmailExists('nsalim@usiu.ac.ke')).toBe("Email already exists!");
 });      
+
+test('checkNameExists: eadwera does not exist', () => {
+    expect(checkNameExists('eadwera')).toBe("Name doesn't exist!");
+});
+test('checkNameExists: nsalim does exist', () => {
+    expect(checkNameExists('nsalim')).toBe("Name already exists!");
+});
