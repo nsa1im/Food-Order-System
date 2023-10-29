97a7fd2ab480ee17e6f51a1f646cd74c39f7b665
m4ster-shifu <145468371+m4ster-shifu@users.noreply.github.com>
2023-10-29 20:38:51 +0300

    Test password function

--- validate.test.js
+++ validate.test.js
@@ -1,4 +1,4 @@
-const {checkNameExists, checkEmailExists, checkEmailUSIU} = require('./validate');     
+const {checkNameExists, checkEmailExists, checkEmailUSIU, checkPassword} = require('./validate');     
 
 test('checkNameExists: eadwera does not exist', () => {
     expect(checkNameExists('eadwera')).toBe("Name doesn't exist!");
@@ -20,3 +20,13 @@ test('checkEmailUSIU: nsalim@usiu.ac.ke is the right format', () => {
 test('checkEmailUSIU: nsalim@google.com is the wrong format', () => {
     expect(checkEmailUSIU('nsalim@google.com')).toBe("Invalid email!");
 }); 
+
+test('checkPassword: 1234 is less than 8 characters long', () => {
+    expect(checkPassword('1234')).toBe("Wrong!");
+});
+test('checkPassword: 12345678 is 8 characters long', () => {
+    expect(checkPassword('12345678')).toBe("Right!");
+}); 
+test('checkPassword: 123456789 is more than 8 characters long', () => {
+    expect(checkPassword('123456789')).toBe("Right!");
+});
