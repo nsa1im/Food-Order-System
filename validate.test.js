fb5c71b4c780683cf3cfd79bf27502292a7b5155
NSALIM <100875129+nsa1im@users.noreply.github.com>
2023-10-29 20:22:14 +0300

    Test checkEmailUSIU function

--- validate.test.js
+++ validate.test.js
@@ -1,15 +1,22 @@
-const {checkEmailExists, checkNameExists} = require('./validate');
+const {checkNameExists, checkEmailExists, checkEmailUSIU} = require('./validate');     
+
+test('checkNameExists: eadwera does not exist', () => {
+    expect(checkNameExists('eadwera')).toBe("Name doesn't exist!");
+});
+test('checkNameExists: nsalim does exist', () => {
+    expect(checkNameExists('nsalim')).toBe("Name already exists!");
+});  
 
 test('checkEmailExists: thisemail@usiu.ac.ke does not exist', () => {
     expect(checkEmailExists('thisemail@usiu.ac.ke')).toBe("Email doesn't exist!");
 });
 test('checkEmailExists: nsalim@usiu.ac.ke does exist', () => {
     expect(checkEmailExists('nsalim@usiu.ac.ke')).toBe("Email already exists!");
-});      
+}); 
 
-test('checkNameExists: eadwera does not exist', () => {
-    expect(checkNameExists('eadwera')).toBe("Name doesn't exist!");
+test('checkEmailUSIU: nsalim@usiu.ac.ke is the right format', () => {
+    expect(checkEmailUSIU('nsalim@usiu.ac.ke')).toBe("Valid email!");
 });
-test('checkNameExists: nsalim does exist', () => {
-    expect(checkNameExists('nsalim')).toBe("Name already exists!");
-});
+test('checkEmailUSIU: nsalim@google.com is the wrong format', () => {
+    expect(checkEmailUSIU('nsalim@google.com')).toBe("Invalid email!");
+}); 
