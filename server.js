03678a99a2c2a6d2d36ed6417b5c91ed6716eaa2
NSALIM <100875129+nsa1im@users.noreply.github.com>
2023-10-29 15:26:29 +0300

    Add email verification function

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 const { v4: uuidv4 } = require('uuid');
+const {checkEmailNotExists} = require('./validate');
 
 app.set('views', 'views');
 app.set('view engine', 'hbs');
@@ -19,25 +20,48 @@ app.post('/overviewPage', urlEncodedParser, function (request, response) {
   const email = request.body.email;
   const password = request.body.password;
   const room = parseInt(request.body.room);
-
-  if(name && email && password && room){
-    const user = {
-      name: name,
-      email: email,
-      password: password,
-      room: room
-    };
-    //check if exists into database
-    //save the details into the database
-
-    response.render('overviewPage', user);
-
-  } else {
+  const user = {
+    name: name,
+    email: email,
+    password: password,
+    room: room
+  };
+  let verify_Email = checkEmailNotExists(email);
+  if (verify_Email==="Email already exists!"){
     response.render('home', {
-      errorMessage: 'Details not entered!',
+      errorMessage: 'Email aready exists.',
     });
   }
+  else if (verify_Email==="Email doesn't exist!"){
+    response.render('overviewPage');
+  }
+  //check if email is USIU email
+  //check if password is 8 characters
+  //check if room is valid
 });
+
+function saveUser(event) {
+  fs.readFile('users.json', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      let userDetails = [];
+      if (data) {
+        userDetails = JSON.parse(data);
+      }
+      event.id = uuidv4();
+      userDetails.push(event);
+      const json = JSON.stringify(userDetails, null, 2);
+      fs.writeFile('users.json', json, 'utf8', function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('User details saved.');
+        }
+      });
+    }
+  });
+}
 
 app.get('/overviewPage', urlEncodedParser, function (request, response) {
   const email = request.body.email;
